8b1f35a5251378b48c33a385a943c297eb699a1d
SalmanHaydar <salmanhaydar3@gmail.com>
2021-12-05 00:15:20 +0100

    schema definition added in database file

--- database.js
+++ database.js
@@ -1,6 +1,24 @@
 var mysql = require('mysql2')
 require('dotenv').config()
 
+//*******************************DB SCHEMA*************************
+// CREATE TABLE users (
+//   id int default null,
+//   full_name varchar(45) DEFAULT NULL,
+//   street_name varchar(45) DEFAULT NULL,
+//   city_name varchar(45) DEFAULT NULL,
+//   postal_code int default null
+// ) ;
+
+
+// CREATE TABLE login (
+//   id int not null ,
+//   user_name varchar(45) DEFAULT NULL,
+//   password varchar(256) DEFAULT NULL,
+//   email varchar(100) DEFAULT NULL
+// ) ;
+
+
 var connection = mysql.createConnection({
   host: process.env.DB_HOST,
   insecureAuth:true,
